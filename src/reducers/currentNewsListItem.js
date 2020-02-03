b5c951e6b131c03973fab96cf29fb96eb6536a25
karasb888 <Ivan.mazurenko99@gmail.com>
2020-02-03 08:35:16 +0200

    Fix current item reducer

--- src/reducers/currentNewsListItem.js
+++ src/reducers/currentNewsListItem.js
@@ -2,7 +2,7 @@ import { SET_CURRENT_NEWS_ITEM } from './../constants/actionTypes';
 
 const currentNewsListItem = (state = null, action) => {
     if (action.type === SET_CURRENT_NEWS_ITEM) {
-        return action.payload || state;
+        return Object.assign({}, action.payload);
     }
     return state;
 };
